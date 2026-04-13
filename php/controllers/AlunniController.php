c6e3f2507e6425dca42ac3c11f7f59582d8c3970
student <student@itismeucci.com>
2026-04-13 10:48:01 +0200

    Initial commit

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -4,12 +4,67 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AlunniController
 {
-  public function index(Request $request, Response $response, $args){
+  public function index(Request $request, Response $response, $args) {
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $result = $mysqli_connection->query("SELECT * FROM alunni");
     $results = $result->fetch_all(MYSQLI_ASSOC);
 
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function coca(Request $request, Response $response, $args) {
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+
+    $stmt = $mysqli_connection->prepare("SELECT * FROM alunni WHERE id = ?");
+    $stmt->bind('i', (int)$args['id']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $results = $result->fetch_array(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+  
+  public function ina(Request $request, Response $response, $args) {
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $data = json_decode($request->getBody(), true);
+    $nomeBello = $data['nome'];
+    $cognomeBello = $data['cognome'];
+
+    $stmt = $mysqli_connection->prepare("INSERT INTO alunni(nome, cognome) VALUES (?, ?)");
+    $stmt->bind('ss', $nomeBello, $cognomeBello);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    return $response->withStatus(200);
+  }
+
+  public function mara(Request $request, Response $response, $args) {
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $data = json_decode($request->getBody(), true);
+    $id = $args['id'];
+    $nomeBello = $data['nome'];
+    $cognomeBello = $data['cognome'];
+
+    $stmt = $mysqli_connection->prepare("UPDATE alunni SET nome = ?, cognome = ? WHERE id = ?");
+    $stmt->bind('ssi', $nomeBello, $cognomeBello, $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    return $response->withStatus(200);
+  }
+
+  public function dona(Request $request, Response $response, $args) {
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $stmt = $mysqli_connection->prepare("DELETE FROM alunni WHERE id = ?");
+    $stmt->bind('i', $args['id']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if (!$result) {
+      return $response->withStatus(404);
+    }
+    return $response->withStatus(200);
   }
 }
--- php/index.php
+++ php/index.php
@@ -8,18 +8,11 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
-
-$app->get('/test', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Test page");
-    return $response;
-});
-
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
+ 
 $app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:coca");
+$app->post('/alunni', "AlunniController:ina");
+$app->put('/alunni/{id}', "AlunniController:mara");
+$app->delete('/alunni/{id}', "AlunniController:dona");
 
 $app->run();
